f895a7d494b40735511839bfec530fb51dbffea1
Bhoot Biswas <bhoot.biswas@gmail.com>
2021-09-08 15:59:53 +0600

    Fix minor issues

--- includes/ajax-hooks.php
+++ includes/ajax-hooks.php
@@ -4,12 +4,12 @@ add_action( 'wp_ajax_nopriv_get_graph_performance_by_year', 'stratusx_child_get_
 
 function stratusx_child_get_graph_performance_by_year_via_ajax() {
 	$portfolio_id = isset( $_POST['portfolio_id'] ) ? sanitize_text_field( $_POST['portfolio_id'] ) : '';
-	$filter_year  = isset( $_POST['filter_year'] ) ? absint( $_POST['filter_year'] ) : 0;
+	$filter_year  = isset( $_POST['filter_year'] ) ? absint( $_POST['filter_year'] ) : date( 'Y' );
 
 	$graph_performance = json_decode( stratusx_child_get_graph_performance( $portfolio_id, $filter_year ) );
 	$graph_performance = $graph_performance->data[0];
 
-	$performance_detail = stratusx_child_get_performance_detail( $portfolio_id, $current_year );
+	$performance_detail = stratusx_child_get_performance_detail( $portfolio_id, $filter_year );
 	$performance_detail = $performance_detail->data[0];
 
 	$performance_detail_html = '';
